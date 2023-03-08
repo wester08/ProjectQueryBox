4d845a9459596146d95cbf1a35106f5e7c08e080
wester08~^C <wester.feliz08@gmail.com>
2023-03-08 12:32:51 -0400

    Primera version

---
+++ src/main/java/Localizadores.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.By;
+
+public class Localizadores {
+    By EncabezadoPagina = By.xpath("/html/body/app-root/nb-layout-column/app-auth/div/div/div/form/span");
+    By CampoUsuario = By.name("userName");
+    By CampoContrasena = By.name("password");
+    By BtnIngresar = By.xpath("/html/body/app-root/nb-layout-column/app-auth/div/div/div/form/div[3]/button");
+    By HomePage = By.xpath("/html/body/app-root/nb-layout-column/app-home/nb-layout/div/div/nb-layout-header/nav/div/div/img");
+    By MjsCredencialesIncorrectas = By.xpath("/html/body/app-root/nb-layout-column/app-auth/div/div/div/form/span[2]");
+
+    By DropdownSistema = By.name("sistema");
+
+}
---
+++ src/test/java/CredencialesIncorrectas.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CredencialesIncorrectas extends Localizadores {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\wester.feliz\\Downloads\\Proyectos 02-11-2022\\QueryBox\\src\\main\\resources\\Driver\\msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("http://10.1.0.7:9402/");
+
+    }
+
+    @Test
+    public void login() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.findElement(EncabezadoPagina).isDisplayed();
+        driver.findElement(CampoUsuario).sendKeys("401399");
+        driver.findElement(CampoContrasena).sendKeys("401399--");
+        driver.findElement(BtnIngresar).click();
+        Thread.sleep(1000);
+        driver.findElement(MjsCredencialesIncorrectas).isDisplayed();
+
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+
+    }
+}
